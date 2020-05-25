5cae76acbf94b254c36c7613dfc475c4b2b1dac4
JiamingRen <50855037+JiamingRen@users.noreply.github.com>
2020-05-25 20:37:07 +1000

    read the finger movement direction

--- FlyingBird/app/src/main/java/com/demo/flyingbrid/GameView.java
+++ FlyingBird/app/src/main/java/com/demo/flyingbrid/GameView.java
@@ -2,6 +2,8 @@ package com.demo.flyingbrid;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.GridLayout;
 
 public class GameView extends GridLayout {
@@ -19,6 +21,41 @@ public class GameView extends GridLayout {
         initGameView();
     }
     private void initGameView(){
+        //set on touch listener to read player's finger movement
+        setOnTouchListener(new View.OnTouchListener() {
+            // get
+            private float sX,sY,offsetX,offsetY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        //get the position when user click
+                        sX = event.getX();
+                        sY = event.getY();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        // get the move distance when finger off the screen
+                        offsetX = event.getX()-sX;
+                        offsetY = event.getY()-sY;
+                        if (Math.abs(offsetX)>Math.abs(offsetY)) {
+                            //decide the moving directions
+                            if (offsetX<-5) {
+                                System.out.println("left");
+                            }else if (offsetX>5) {
+                                System.out.println("right");
+                            }
+                        }else{
+                            if (offsetY<-5) {
+                                System.out.println("up");
+                            }else if (offsetY>5) {
+                                System.out.println("down");
+                            }
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
 
     }
 }
