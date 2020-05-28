8a1ddd033a18234cb6d052b91ae2f0f6f4a8abee
JiamingRen <50855037+JiamingRen@users.noreply.github.com>
2020-05-29 00:03:33 +1000

    initial variables

--- FlyingBird/app/src/main/java/com/demo/flyingbrid/GameView.java
+++ FlyingBird/app/src/main/java/com/demo/flyingbrid/GameView.java
@@ -2,15 +2,38 @@ package com.demo.flyingbrid;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.GridLayout;
 
+import java.util.Random;
+
 public class GameView extends View {
     //4x4 map
     public static final int count = 4;
+    //create standard variables
+    private int screen_width;
+    private int screen_height;
+    private Paint pain;
+    private float paint_width;
+    private float paint_height;
+    private int startX;
+    private int startY;
+    private int block_size;
+    private int [][] map;
+    private float x_click;
+    private float y_click;
+    private float x_move;
+    private float y_move;
+    private boolean Pressed;
+    private boolean Moved;
+    private Random random;
+    private int scores;
+    
+    //initial colors
     private static SparseIntArray blocks_colors;
     static {
         blocks_colors = new SparseIntArray();
