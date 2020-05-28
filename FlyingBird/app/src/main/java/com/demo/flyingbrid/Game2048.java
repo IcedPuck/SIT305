ac29fb8d3993db383933bab0d3c0b191146f5dd4
JiamingRen <50855037+JiamingRen@users.noreply.github.com>
2020-05-29 02:33:38 +1000

    load game view

--- FlyingBird/app/src/main/java/com/demo/flyingbrid/Game2048.java
+++ FlyingBird/app/src/main/java/com/demo/flyingbrid/Game2048.java
@@ -1,22 +1,32 @@
 package com.demo.flyingbrid;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.MotionEvent;
 import android.view.View;
 
 public class Game2048 extends AppCompatActivity {
+    private GameView gameView;
+    private AlertDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game2);
+        gameView = new GameView(this);
+        setContentView(gameView);
 
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gameView.onTouchEvent(event);
+    }
 }
