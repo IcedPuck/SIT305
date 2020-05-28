625348256380267daaeda6cb0696d851f8a0a775
JiamingRen <50855037+JiamingRen@users.noreply.github.com>
2020-05-29 00:36:52 +1000

    append scores

--- FlyingBird/app/src/main/java/com/demo/flyingbrid/GameView.java
+++ FlyingBird/app/src/main/java/com/demo/flyingbrid/GameView.java
@@ -1,13 +1,18 @@
 package com.demo.flyingbrid;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.GridLayout;
+
+import androidx.annotation.Nullable;
 
 import java.util.Random;
 
@@ -17,7 +22,7 @@ public class GameView extends View {
     //create standard variables
     private int screen_width;
     private int screen_height;
-    private Paint pain;
+    private Paint paint;
     private float paint_width;
     private float paint_height;
     private int startX;
@@ -32,7 +37,7 @@ public class GameView extends View {
     private boolean Moved;
     private Random random;
     private int scores;
-    
+
     //initial colors
     private static SparseIntArray blocks_colors;
     static {
@@ -50,7 +55,57 @@ public class GameView extends View {
         blocks_colors.put(2048, Color.parseColor("#EFC329"));
         blocks_colors.put(4096, Color.parseColor("#FF3C39"));
     }
-    public GameView(Context context) {
-        super(context);
+
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        this.paint = new Paint();
+        this.paint_width = 15f;
+        this.random = new Random();
+        this.map = new int[count][count];
+        for (int i = 0; i < 3; i++){
+            createRandomBlock();
+        }
+        WindowManager windowManager = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(outMetrics);
+        screen_width = outMetrics.widthPixels;
+        screen_height = outMetrics.heightPixels;
+
+        startY = (screen_height - screen_width) >> 1;
+        startY = 0;
+
+        block_size = screen_width / count;
     }
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        gameArea();
+        gameRect();
+        drawScore();
+    }
+    // draw parts
+    private void drawScore(Canvas canvas, int x, int y) {
+        // reset when game start
+        paint.reset();
+        paint.setColor(Color.GRAY);
+        paint.setTextSize(block_size * 5/ 12);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setStrokeWidth(paint_width);
+        canvas.drawText("Score: " + scores, screen_width/2, startY/2, paint);
+    }
+
+    private void gameRect() {
+    }
+
+    private void gameArea() {
+    }
+
+    private void createRandomBlock() {
+    }
+//    public GameView(Context context) {
+//        super(context, null);
+//    }
+
 }
