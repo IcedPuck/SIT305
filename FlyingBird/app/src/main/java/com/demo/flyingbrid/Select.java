f516dd0d70ab7e8db0861b44cf5d95664eaad8f2
IcedPuck <645612410@qq.com>
2020-06-03 11:04:21 +0800

    add land screen

--- FlyingBird/app/src/main/java/com/demo/flyingbrid/Select.java
+++ FlyingBird/app/src/main/java/com/demo/flyingbrid/Select.java
@@ -1,8 +1,11 @@
 package com.demo.flyingbrid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
